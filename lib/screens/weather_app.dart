e854a7f7dc9dd9ca2fd93920d91453e5a171788d
Tarkan Kara <unnahmet23@hotmail.com>
2022-10-28 22:51:18 +0300

    Slider Dots

--- lib/screens/weather_app.dart
+++ lib/screens/weather_app.dart
@@ -1,8 +1,8 @@
-// ignore_for_file: avoid_unnecessary_containers
+// ignore_for_file: avoid_unnecessary_containers, depend_on_referenced_packages
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:weather_app/widgets/single_weather.dart';
 
 class WeatherApp extends StatelessWidget {
   const WeatherApp({super.key});
@@ -54,233 +54,57 @@ class WeatherApp extends StatelessWidget {
               ),
             ),
             Container(
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const SizedBox(height: 120),
-                              Text(
-                                "ISTANBUL",
-                                style: GoogleFonts.lato(
-                                  fontSize: 35,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              const SizedBox(height: 5),
-                              Text(
-                                "07.50 PM - Pazartesi, 28 Ekim 2022",
-                                style: GoogleFonts.lato(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "24\u2103",
-                                style: GoogleFonts.lato(
-                                  fontSize: 85,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              Row(
-                                children: [
-                                  SvgPicture.asset(
-                                    "assets/moon.svg",
-                                    height: 30,
-                                    width: 30,
-                                    color: Colors.white,
-                                  ),
-                                  const SizedBox(width: 15),
-                                  Text(
-                                    "Gece",
-                                    style: GoogleFonts.lato(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w500,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ],
+              margin: const EdgeInsets.fromLTRB(20, 120, 0, 0),
+              child: Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 5),
+                    width: 18,
+                    height: 8,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5),
                       ),
                     ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(vertical: 30),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.white30,
-                        ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 5),
+                    width: 18,
+                    height: 8,
+                    decoration: const BoxDecoration(
+                      color: Colors.white54,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5),
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(5, 0, 5, 10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            children: [
-                              Text(
-                                "Rüzgar",
-                                style: GoogleFonts.lato(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              const SizedBox(height: 5),
-                              Text(
-                                "10",
-                                style: GoogleFonts.lato(
-                                  fontSize: 22,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              const SizedBox(height: 5),
-                              Text(
-                                "km/h",
-                                style: GoogleFonts.lato(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              const SizedBox(height: 5),
-                              Stack(
-                                children: [
-                                  Container(
-                                    height: 5,
-                                    width: 50,
-                                    color: Colors.white38,
-                                  ),
-                                  Container(
-                                    height: 5,
-                                    width: 10,
-                                    color: Colors.greenAccent,
-                                  ),
-                                ],
-                              ),
-                              const SizedBox(height: 5),
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              Text(
-                                "Yagmur",
-                                style: GoogleFonts.lato(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              const SizedBox(height: 5),
-                              Text(
-                                "10",
-                                style: GoogleFonts.lato(
-                                  fontSize: 22,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              const SizedBox(height: 5),
-                              Text(
-                                "%",
-                                style: GoogleFonts.lato(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              const SizedBox(height: 5),
-                              Stack(
-                                children: [
-                                  Container(
-                                    height: 5,
-                                    width: 50,
-                                    color: Colors.white38,
-                                  ),
-                                  Container(
-                                    height: 5,
-                                    width: 20,
-                                    color: Colors.greenAccent,
-                                  ),
-                                ],
-                              ),
-                              const SizedBox(height: 5),
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              Text(
-                                "Nem",
-                                style: GoogleFonts.lato(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              const SizedBox(height: 5),
-                              Text(
-                                "10",
-                                style: GoogleFonts.lato(
-                                  fontSize: 22,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              const SizedBox(height: 5),
-                              Text(
-                                "%",
-                                style: GoogleFonts.lato(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              const SizedBox(height: 5),
-                              Stack(
-                                children: [
-                                  Container(
-                                    height: 5,
-                                    width: 50,
-                                    color: Colors.white38,
-                                  ),
-                                  Container(
-                                    height: 5,
-                                    width: 30,
-                                    color: Colors.red,
-                                  ),
-                                ],
-                              ),
-                              const SizedBox(height: 5),
-                            ],
-                          ),
-                        ],
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 5),
+                    width: 18,
+                    height: 8,
+                    decoration: const BoxDecoration(
+                      color: Colors.white54,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 5),
+                    width: 18,
+                    height: 8,
+                    decoration: const BoxDecoration(
+                      color: Colors.white54,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            )
+            ),
+            const SingleWeather(),
           ],
         ),
       ),
---
+++ lib/widgets/single_weather.dart
@@ -0,0 +1,239 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SingleWeather extends StatelessWidget {
+  const SingleWeather({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 120),
+                      Text(
+                        "ISTANBUL",
+                        style: GoogleFonts.lato(
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        "07.50 PM - Pazartesi, 28 Ekim 2022",
+                        style: GoogleFonts.lato(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "24\u2103",
+                        style: GoogleFonts.lato(
+                          fontSize: 85,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          SvgPicture.asset(
+                            "assets/moon.svg",
+                            height: 30,
+                            width: 30,
+                            color: Colors.white,
+                          ),
+                          const SizedBox(width: 15),
+                          Text(
+                            "Gece",
+                            style: GoogleFonts.lato(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 30),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.white30,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(5, 0, 5, 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      Text(
+                        "Rüzgar",
+                        style: GoogleFonts.lato(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        "10",
+                        style: GoogleFonts.lato(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        "km/h",
+                        style: GoogleFonts.lato(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Stack(
+                        children: [
+                          Container(
+                            height: 5,
+                            width: 50,
+                            color: Colors.white38,
+                          ),
+                          Container(
+                            height: 5,
+                            width: 10,
+                            color: Colors.greenAccent,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 5),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        "Yagmur",
+                        style: GoogleFonts.lato(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        "10",
+                        style: GoogleFonts.lato(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        "%",
+                        style: GoogleFonts.lato(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Stack(
+                        children: [
+                          Container(
+                            height: 5,
+                            width: 50,
+                            color: Colors.white38,
+                          ),
+                          Container(
+                            height: 5,
+                            width: 20,
+                            color: Colors.greenAccent,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 5),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        "Nem",
+                        style: GoogleFonts.lato(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        "10",
+                        style: GoogleFonts.lato(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        "%",
+                        style: GoogleFonts.lato(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Stack(
+                        children: [
+                          Container(
+                            height: 5,
+                            width: 50,
+                            color: Colors.white38,
+                          ),
+                          Container(
+                            height: 5,
+                            width: 30,
+                            color: Colors.red,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 5),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
