34b41168221b867c2515813ec92e2bca086f8daf
Tarkan Kara <unnahmet23@hotmail.com>
2022-10-28 20:53:52 +0300

    project setup

---
+++ lib/screens/weather_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
